96d9a5333f12a2ecb7a5312bbb6fc6caea0721dc
FaQ <fcostazini@gmail.com>
2016-03-13 02:36:46 -0300

    Agrego manejo de errores

--- cuentas-claras/www/js/controllers.js
+++ cuentas-claras/www/js/controllers.js
@@ -615,27 +615,7 @@ angular.module('starter.controllers', ['ionic', 'ionMdInput'])
 
 
   })
-  .controller('DetalleCategoriaCtrl', function ($scope, ionicMaterialInk, $timeout, ionicMaterialMotion, categoriasService) {
-    $scope.$parent.showHeader();
-    $scope.$parent.clearFabs();
-    $scope.isExpanded = true;
-    $scope.$parent.setExpanded(true);
-    $scope.$parent.setHeaderFab('right');
-    $scope.categoriaSeleccionada = categoriasService.getSeleccionada();
 
-
-    $timeout(function () {
-      ionicMaterialMotion.fadeSlideIn({
-        selector: '.animate-fade-slide-in .item'
-      });
-
-    }, 200);
-
-
-    // Activate ink for controller
-    ionicMaterialInk.displayEffect();
-
-  });
 
 
 
