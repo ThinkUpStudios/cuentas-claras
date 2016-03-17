bb6af830faa912f17263e410855e23ead2721fc0
Facundo Costa Zini <fcostazini@conexia.com>
2016-03-17 16:26:29 -0300

    Cambio titulo de editarCategoria

--- cuentas-claras/www/js/ads.js
+++ cuentas-claras/www/js/ads.js
@@ -6,9 +6,8 @@
   // Determine platform
   if (/(android)/i.test(navigator.userAgent)) {
     admobData = {
-      //banner: "ca-app-pub-7515525101165816/8899419086"
+      banner: "ca-app-pub-7515525101165816/8899419086"
 
-      banner: "ca-app-pub-3940256099942544/6300978111"
     };
   } else if (/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
     admobData = {
